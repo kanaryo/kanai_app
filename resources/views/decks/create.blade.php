d6845afe13075829f4ef72d75d5abe71be1abe2e
canitenda78 <canitenda78@gmail.com>
2025-04-04 20:25:49 +0900

    新規デッキ作成画面にデザインを追加

--- resources/views/decks/create.blade.php
+++ resources/views/decks/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>新規デッキ作成</h1>
-    <form action="{{ route('decks.store') }}" method="POST">
+<h1 class="form-title">新規デッキ作成</h1>
+<form action="{{ route('decks.store') }}" method="POST" class="form-container">
         @csrf
         <label for="name">デッキ名:</label>
-        <input type="text" id="name" name="name" required>
-        <button type="submit">作成</button>
+        <input type="text" id="name" name="name" required class="input-custom">
+        <button type="submit" class="submit-button">作成</button>
     </form>
 @endsection
